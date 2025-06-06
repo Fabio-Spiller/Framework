12364b63f63eb50bb5eeaa3ebbe634b526a43047
Fabio-Spiller <devfabiospiller@gmail.com>
2025-06-06 15:23:34 -0300

    init

---
+++ Sorvetinho/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <button type="button" class="btn btn-primary">Olá Mundão!</button>
+    
+  )
+}
+
+export default App
